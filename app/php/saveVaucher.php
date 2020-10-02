0422a2f158a900cc54f6b88e3a08ea7740a15709
bio0z <bezsnow@gmail.com>
2020-10-02 11:12:48 +0300

    FIX add info to vaucher

--- app/php/saveVaucher.php
+++ app/php/saveVaucher.php
@@ -12,9 +12,18 @@ if(isset($_POST)) {
   $ar['passname'] = $_POST['passname'] == 'S' ? 'STANDARD TOUR' : 'VIP TOUR';
   $ar['guest1'] = $_POST['name'] . ' ' . $_POST['lastname'];
   $ar['guest2'] = $_POST['gname'] . ' ' . $_POST['glastname'];
-  $ar['room'] = $_POST['room'] == 'S' ? 'Single,  1 гость' : 'Double, 2 гостя';
   $ar['hotel'] = $_POST['hotel'];
+  $ar['dateFrom'] = $_POST['dateFrom'];
+  $ar['dateTill'] = $_POST['dateTill'];
+  $ar['adults'] = $_POST['adults'];
+  $ar['kids'] = $_POST['kids'];
+  $ar['hotelName'] = $_POST['hotelName'];
+  $ar['hotelBreakfast'] = $_POST['hotelBreakfast'];
+  $ar['hotelPrices'] = $_POST['hotelPrices'];
   $ar['address'] = $_POST['address'];
+  $ar['room'] = $_POST['room'];
+  $ar['tourPrice'] = $_POST['tourPrice'];
+  $ar['tourDays'] = $_POST['tourDays'];
 
   $vaucher = json_encode($ar);
 
