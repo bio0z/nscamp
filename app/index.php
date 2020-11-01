a93b43eab2a76c8e3dc6c898cc9da28302b41e55
bio0z <bezsnow@gmail.com>
2020-11-01 17:41:28 +0300

    ADD js version

--- app/index.php
+++ app/index.php
@@ -1213,7 +1213,7 @@ $env = $host == 'nswpay.ru' ? 'prod' : 'test';
 <script src="https://unpkg.com/vuejs-datepicker"></script>
 <script src="https://unpkg.com/vue-router"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="<?= $path ?>js/app.min.js?rev=7.7"></script>
+<script src="<?= $path ?>js/app.min.js?rev=7.8"></script>
 <!--<script src="js/vue-tap.js"></script>-->
 <!--<script src="js/vue-touch-events.js"></script>-->
 <?php if ($env == 'prod') { ?>
