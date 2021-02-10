bfbe167503d57d4a04d84466864036238bc0722f
iobez <bezsnow@gmail.com>
2021-02-10 18:35:09 +0300

    FIX - double = triple

--- app/index.php
+++ app/index.php
@@ -954,7 +954,7 @@ $env = $host == 'nswpay.ru' ? 'prod' : 'test';
 <script src="https://unpkg.com/vuejs-datepicker"></script>
 <script src="https://unpkg.com/vue-router"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="<?= $path ?>js/app.min.js?rev=1612943647"></script>
+<script src="<?= $path ?>js/app.min.js?rev=1612965350"></script>
 <!--<script src="js/vue-tap.js"></script>-->
 <!--<script src="js/vue-touch-events.js"></script>-->
 <?php if ($env == 'prod') { ?>
